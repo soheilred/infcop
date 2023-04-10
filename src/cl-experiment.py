bf0d5127ed3ba0ba824aaedadfa88a40dae78da7
Soheil Gharatappeh <soheilred@yahoo.com>
2023-04-10 10:46:28 -0400

    creating a simple cl experiment

--- src/cl-experiment.py
+++ src/cl-experiment.py
@@ -1,7 +1,46 @@
 #!/usr/bin/env python
+import utils
+from data_loader import Data
+from network import Network, train, test
+from correlation import Activations
+import constants as C
+from prune import Pruner
+import logging
+import logging.config
+import torch
+import torch.nn as nn
+
 
 def main():
-    pass
+    device = utils.get_device()
+    args = utils.get_args()
+    logger = utils.setup_logger()
+
+    # instantiate the model 
+    network = Network(device, args.arch)
+    preprocess = network.preprocess
+    model = network.set_model()
+    loss_fn = nn.CrossEntropyLoss()
+
+    dataset_list = ["CIFAR10", "MNIST"]
+
+    # Looping over tasks
+    for task in range(0, args.task_num):
+        # Update the dataset
+        data = Data(args.batch_size, C.DATA_DIR, dataset_list[task])
+        num_classes = data.get_num_classes()
+        train_dl, test_dl = data.train_dataloader, data.test_dataloader
+
+        # prepare the model for the task
+        model.add_dataset(str(task), num_classes)
+        model.set_dataset(str(task))
+
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+        train_acc, _ = train(model, train_dl, loss_fn, optimizer,
+                             args.train_epochs, device)
+        pruning = Pruner(model, args.arch, args.prune_perc_per_layer*100,
+                            train_dl, test_dl)
+
 
 if __name__ == '__main__':
     main()
