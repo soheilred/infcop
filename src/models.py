cc77c00fc8583c28e6678382c60238180a31ad5b
Soheil Gharatappeh <soheilred@yahoo.com>
2023-04-11 11:33:55 -0400

    update

--- src/models.py
+++ src/models.py
@@ -396,25 +396,6 @@ class ResNet(nn.Module):
         print('Check passed')
         raw_input()
 
-
-
-
-class fc1(nn.Module):
-
-    def __init__(self, num_classes=10):
-        super(fc1, self).__init__()
-        self.classifier = nn.Sequential(
-            nn.Linear(3*32*32, 300),
-            nn.ReLU(inplace=True),
-            nn.Linear(300, 100),
-            nn.ReLU(inplace=True),
-            nn.Linear(100, num_classes),
-        )
-
-    def forward(self, x):
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
 def _resnet(arch, block, layers, progress, device, **kwargs):
     print("Start")
     model = ResNet(block, layers, num_classes=10, **kwargs)
@@ -444,3 +425,21 @@ def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+
+
+class fc1(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(fc1, self).__init__()
+        self.classifier = nn.Sequential(
+            nn.Linear(3*32*32, 300),
+            nn.ReLU(inplace=True),
+            nn.Linear(300, 100),
+            nn.ReLU(inplace=True),
+            nn.Linear(100, num_classes),
+        )
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
