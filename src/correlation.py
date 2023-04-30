493a52628d862e553788b8298ef0b354a54a10b8
Soheil Gharatappeh <soheilred@yahoo.com>
2023-04-30 09:16:24 -0400

    check parent's and child mean and std for nans

--- src/correlation.py
+++ src/correlation.py
@@ -161,16 +161,16 @@ class Activations:
                                   detach().cpu().numpy())
                     child.append(self.activation[act_keys[idx + 1]].\
                                  detach().cpu().numpy())
-                    if np.any(np.isnan(parent[batch])):
-                        print("nan in layer {layers_idx[idx]}")
-
-                    if np.any(np.isnan(child[batch])):
-                        print("nan in layer {layers_idx[idx + 1]}")
 
             parent = np.vstack(parent)
             parent = (parent - parent.mean(axis=0)) / parent.std(axis=0)
             child = np.vstack(child)
             child = (child - child.mean(axis=0)) / child.std(axis=0)
+            if np.any(np.isnan(parent)):
+                print("nan in layer {layers_idx[idx]}")
+
+            if np.any(np.isnan(child)):
+                print("nan in layer {layers_idx[idx + 1]}")
             # corr = np.corrcoef(parent, child, rowvar=False)
             # x_len = corr.shape[0] // 2
             # y_len = corr.shape[1] // 2
