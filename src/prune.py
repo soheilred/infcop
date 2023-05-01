c996e2efe061c3ee712daef26fa77f21ac09cd3e
Soheil Gharatappeh <soheilred@yahoo.com>
2023-04-30 22:36:28 -0400

    turn max into mean

--- src/prune.py
+++ src/prune.py
@@ -646,7 +646,8 @@ def main():
         # plot_tool.plot_all_accuracy(all_acc, C.OUTPUT_DIR + str(i) +
         #                             "all_accuracies")
 
-    all_acc = np.mean(np.max(acc_list, axis=2), axis=0)
+    # all_acc = np.mean(np.max(acc_list, axis=2), axis=0)
+    all_acc = np.mean(acc_list, axis=0)
     conn = np.mean(conn_list, axis=0)
     # plot_tool.plot_all_accuracy(all_acc, C.OUTPUT_DIR + "all_accuracies")
     utils.save_vars(save_dir=run_dir, conn=conn, all_accuracies=all_acc)
