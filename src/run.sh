f438123986791cff9c019300d303eefbcbbd8f67
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 17:41:25 -0400

    adding thrd flag

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-touch ../output/errors/batch-run.out
+# touch ../output/errors/batch-run.out
 
 ARCHS=("alexnet" "vgg16" "resnet18")
 DATASETS=("IMAGENET" "MNIST") #"CIFAR10" 
@@ -9,10 +9,10 @@ for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=-1 --control_at_epoch=-1 \
         --control_at_layer="2" --experiment_type=efficiency \
-        >> ../output/errors/batch-run.out
+        --acc_thrd=90 >> ../output/errors/batch-run.out
 
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
         --control_at_layer="2" --experiment_type=efficiency \
-        >> ../output/errors/batch-run.out
+        --acc_thrd=90 >> ../output/errors/batch-run.out
 done; done;
