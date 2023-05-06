eb091209cc4ae432ab956ec2bfa7afc60b734ebe
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 18:04:07 -0400

    minor

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,10 @@
 
 # touch ../output/errors/batch-run.out
 
+LOG="../output/errors/batch-run.out"
+date >> $LOG
+echo $1 >> $LOG
+
 ARCHS=("alexnet" "vgg16" "resnet18")
 DATASETS=("IMAGENET" "CIFAR10" "MNIST") # 
 for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
@@ -9,10 +13,10 @@ for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=-1 --control_at_epoch=-1 \
         --control_at_layer="2" --experiment_type=$1 \
-        --acc_thrd=90 >> ../output/errors/batch-run.out
+        --acc_thrd=90 >> $LOG
 
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
         --control_at_layer="2" --experiment_type=$1 \
-        --acc_thrd=90 >> ../output/errors/batch-run.out
+        --acc_thrd=90 >> $LOG
 done; done;
