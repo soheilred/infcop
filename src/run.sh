1ce5dd4de448b1d6baf45ef6dc613fdc6ba1f473
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 14:06:03 -0400

    remove cifar10

--- src/run.sh
+++ src/run.sh
@@ -3,14 +3,16 @@
 touch ../output/errors/batch-run.out
 
 ARCHS=("alexnet" "vgg16" "resnet18")
-DATASETS=("IMAGENET" "CIFAR10" "MNIST")
+DATASETS=("IMAGENET" "MNIST") #"CIFAR10" 
 for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     echo $arch $dataset >> ../output/errors/batch-run.out
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=-1 --control_at_epoch=-1 \
-        --control_at_layer="2" >> ../output/errors/batch-run.out
+        --control_at_layer="2" --experiment_type=efficiency \
+        >> ../output/errors/batch-run.out
 
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
-        --control_at_layer="2" >> ../output/errors/batch-run.out
+        --control_at_layer="2" --experiment_type=efficiency \
+        >> ../output/errors/batch-run.out
 done; done;
