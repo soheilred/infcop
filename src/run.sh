305e92a81a0d0bb483f9831369034b8e5f651317
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 17:57:02 -0400

    efficiency experiment

--- src/run.sh
+++ src/run.sh
@@ -8,11 +8,11 @@ for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     echo $arch $dataset >> ../output/errors/batch-run.out
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=-1 --control_at_epoch=-1 \
-        --control_at_layer="2" --experiment_type=efficiency \
+        --control_at_layer="2" --experiment_type=$1 \
         --acc_thrd=90 >> ../output/errors/batch-run.out
 
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
         --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
-        --control_at_layer="2" --experiment_type=efficiency \
+        --control_at_layer="2" --experiment_type=$1 \
         --acc_thrd=90 >> ../output/errors/batch-run.out
 done; done;
