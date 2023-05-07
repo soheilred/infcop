393d1003eacceb8f1547e5d89ae568bd69041eb3
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 22:45:24 -0400

    debug

--- src/run.sh
+++ src/run.sh
@@ -12,12 +12,12 @@ DATASETS=("IMAGENET" "CIFAR10" "MNIST") #
 for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     echo $arch $dataset >> ../output/errors/batch-run.out
     sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
-        --train_epochs=3 --control_at_iter=-1 --control_at_epoch=-1 \
+        --train_epochs=3 --control_at_iter=-1 --control_at_epoch=2 \
         --control_at_layer="2" --experiment_type=$1 \
         --acc_thrd=90 >> $LOG
 
-    sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
-        --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
-        --control_at_layer="2" --experiment_type=$1 \
-        --acc_thrd=90 >> $LOG
+    # sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
+    #     --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
+    #     --control_at_layer="2" --experiment_type=$1 \
+    #     --acc_thrd=90 >> $LOG
 done; done;
