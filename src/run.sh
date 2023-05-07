52da738221d00a5a5fa49116c9a2feb90407a197
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-06 23:53:17 -0400

    bug

--- src/run.sh
+++ src/run.sh
@@ -16,8 +16,8 @@ for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
         --control_at_layer="2" --experiment_type=$1 \
         --acc_thrd=90 >> $LOG
 
-    # sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
-    #     --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
-    #     --control_at_layer="2" --experiment_type=$1 \
-    #     --acc_thrd=90 >> $LOG
+    sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
+        --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
+        --control_at_layer="2" --experiment_type=$1 \
+        --acc_thrd=90 >> $LOG
 done; done;
