e14c22065ff4446d0f8e0ebbf8d1363ff697c7c1
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-09 12:22:14 -0400

    running both w and w.o. controller at once

--- src/run.sh
+++ src/run.sh
@@ -9,15 +9,15 @@ echo $1 >> $LOG
 
 ARCHS=("alexnet" "vgg16" "resnet18")
 DATASETS=("IMAGENET" "CIFAR10" "MNIST") # 
-for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
+# for arch in ${ARCHS[@]}; do for dataset in ${DATASETS[@]}; do
     echo $arch $dataset >> ../output/errors/batch-run.out
-    sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
-        --train_epochs=3 --control_at_iter=-1 --control_at_epoch=2 \
+    sbatch experiments.slurm --lr=.001 --arch=$2 --dataset=$3 \
+        --train_epochs=$4 --control_at_iter=-1 --control_at_epoch=2 \
         --control_at_layer="2" --experiment_type=$1 \
-        --acc_thrd=90 >> $LOG
+        --acc_thrd=$4 >> $LOG
 
-    sbatch experiments.slurm --lr=.001 --arch=$arch --dataset=$dataset \
-        --train_epochs=3 --control_at_iter=1 --control_at_epoch=2 \
+    sbatch experiments.slurm --lr=.001 --arch=$2 --dataset=$3 \
+        --train_epochs=$4 --control_at_iter=1 --control_at_epoch=2 \
         --control_at_layer="2" --experiment_type=$1 \
-        --acc_thrd=90 >> $LOG
-done; done;
+        --acc_thrd=$4 >> $LOG
+# done; done;
