a8b60e7747f09fb4c49da75343edfc22d73f4ea9
katahdin <soheilred@yahoo.com>
2023-05-23 18:23:58 -0400

    acg results, all

--- src/prune.py
+++ src/prune.py
@@ -296,7 +296,7 @@ class Pruner:
 
             # type 1
             if (self.controller.c_type == 1):
-                control_weights = prev_corr
+                control_weights = abs(prev_corr)
 
             # type 2
             elif (self.controller.c_type == 2):
@@ -304,7 +304,7 @@ class Pruner:
 
             # type 3
             elif (self.controller.c_type == 3):
-                control_weights = connectivity[ind] # * prev_weight
+                control_weights = abs(connectivity[ind]) # * prev_weight
 
             # type 4
             if (self.controller.c_type == 4):
@@ -416,8 +416,8 @@ def perf_lth(logger, device, args, controller):
             if (train_iter == controller.c_epoch) and \
                 (imp_iter == controller.c_iter):
                 act = Activations(model, test_dl, device, args.batch_size)
-                corr = act.get_corrs()
-                # corr = act.get_correlations()
+                # corr = act.get_corrs()
+                corr = act.get_correlations()
                 pruning.control(corr, act.layers_dim, imp_iter)
                 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,
                                              weight_decay=1e-4)
@@ -430,8 +430,8 @@ def perf_lth(logger, device, args, controller):
         # Calculate the connectivity
         # if (imp_iter <= controller.c_iter):
         activations = Activations(model, test_dl, device, args.batch_size)
-        pruning.corrs.append(activations.get_corrs())
-        # pruning.corrs.append(activations.get_correlations())
+        # pruning.corrs.append(activations.get_corrs())
+        pruning.corrs.append(activations.get_correlations())
         connectivity.append(activations.get_conns(pruning.corrs[imp_iter]))
         # utils.save_vars(corrs=pruning.corrs, all_accuracies=pruning.all_acc)
 
