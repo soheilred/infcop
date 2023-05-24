9ee0d0b63aa938ffd6e544733bb37b2f6a76e7d2
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-24 10:28:17 -0400

    some minor changes in plotting

--- src/plot_exper.py
+++ src/plot_exper.py
@@ -8,10 +8,10 @@ import os
 import numpy as np
 
 
-def making_plots_perf(arch, dataset, exper_cntr, exper_no_cntr):
+def making_plots_perf(arch, dataset, layer, exper_cntr, exper_no_cntr):
     print(arch, dataset)
     no_cntr_dir = f"../output/performance/{arch}/{dataset}/no_cntr/" + exper_no_cntr
-    cntr_dir = f"../output/performance/{arch}/{dataset}/cntr/8/" + exper_cntr
+    cntr_dir = f"../output/performance/{arch}/{dataset}/cntr/{layer}/" + exper_cntr
 
     labels = [f"{arch} w. $\phi$", f"{arch} w.o. $\phi$"]
     if not os.path.exists(no_cntr_dir):
@@ -36,9 +36,10 @@ def making_plots_perf(arch, dataset, exper_cntr, exper_no_cntr):
     # print(no_cntr_conn)
 
     plot_tool.plot_multi_all_accuracy([no_cntr_all_acc, cntr_all_acc], labels,
-                                      args, no_cntr_dir + "all_accuracies")
-    plot_tool.plot_connectivity(cntr_conn, no_cntr_dir + "conn")
-    plot_tool.plot_connectivity(no_cntr_conn, no_cntr_dir + "no_conn")
+                                      args, cntr_dir + "all_accuracies")
+    plot_tool.plot_connectivity(cntr_conn, cntr_dir + "conn")
+    plot_tool.plot_connectivity(no_cntr_conn, cntr_dir + "no_conn")
+    print("plots saved in:", cntr_dir)
 
 
 def making_plots_eff(arch, dataset):
@@ -75,13 +76,16 @@ def main():
     ARCHS=["resnet18", "vgg16"]
     DATASETS=["MNIST", "CIFAR10"]
 
-    exper_cntr = ["", "13-32/", "13-38/", "13-56/"]
+    exper_cntr = ["", "13-32/", "13-38/", "13-56/", "08-52/"]
     exper_no_cntr = [""]
+
+    layer = 9
 
     # exper_cntr = ""
     # exper_no_cntr = ""
 
-    making_plots_perf(ARCHS[0], DATASETS[1], exper_cntr[-1], exper_no_cntr[-1])
+    making_plots_perf(ARCHS[0], DATASETS[1], layer, exper_cntr[-1],
+                      exper_no_cntr[-1])
     # making_plots_efficiency(ARCHS[2], DATASETS[1])
     # for arch in ARCHS:
     #     for dataset in DATASETS:
