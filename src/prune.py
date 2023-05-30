b486107e1acbf4e9214f7f07ee2f03f66afcec0f
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-30 06:49:39 -0400

    control at multiple iterations

--- src/prune.py
+++ src/prune.py
@@ -398,7 +398,7 @@ def perf_lth(logger, device, args, controller):
 
             # apply the controller after some epochs and some iterations
             if (train_iter == controller.c_epoch) and \
-                (imp_iter == controller.c_iter):
+                (imp_iter in controller.c_iter):
                 act = Activations(model, test_dl, device, args.batch_size)
                 # corr = act.get_corrs()
                 corr = act.get_correlations()
