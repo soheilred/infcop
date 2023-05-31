0148898865c5b001fbd64b662f02808905302e61
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-31 00:52:52 -0400

    debug

--- src/dir-find.sh
+++ src/dir-find.sh
@@ -4,4 +4,5 @@ ID=$(echo $1 | awk '{print $4}')
 # echo $ID
 DIR="../output/errors/tbst_${ID}.log"
 # echo $DIR
-grep "In dir:" $DIR | awk '{print $6}' | awk -F "/" '{print $9}'
+grep "In dir:" $DIR | awk '{print $6}'
+grep "In dir:" $DIR | awk '{print $6}' | awk -F "/" '{print $11}'
