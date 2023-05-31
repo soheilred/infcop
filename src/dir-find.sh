724686d06624b27fff84f493cac9c3502da7d7c0
Soheil Gharatappeh <soheilred@yahoo.com>
2023-05-31 00:51:58 -0400

    debug

--- src/dir-find.sh
+++ src/dir-find.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ID=$(echo $1 | awk '{print $4}')
-echo $ID
+# echo $ID
 DIR="../output/errors/tbst_${ID}.log"
-echo $DIR
-grep "In dir:" $DIR | awk '{print $6}'
+# echo $DIR
+grep "In dir:" $DIR | awk '{print $6}' | awk -F "/" '{print $9}'
