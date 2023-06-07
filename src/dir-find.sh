86177cc31498e5509d95c6963f9ef41e09bdfa30
katahdin <soheilred@yahoo.com>
2023-06-06 22:08:55 -0400

    acg results

--- src/dir-find.sh
+++ src/dir-find.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ID=$(echo $1 | awk '{print $4}')
-# echo $ID
+echo "PID: "$ID
 DIR="../output/errors/tbst_${ID}.log"
-# echo $DIR
+echo "In Directory:"$DIR
 grep "In dir:" $DIR | awk '{print $6}'
 grep "In dir:" $DIR | awk '{print $6}' | awk -F "/" '{print $11}'
