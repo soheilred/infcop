f4bfad6c40bffc61d23ab11633ec9a0b87edafca
Soheil Gharatappeh <soheilred@yahoo.com>
2023-07-05 02:08:33 -0400

    debug

--- src/plot_exper.py
+++ src/plot_exper.py
@@ -42,14 +42,16 @@ def making_plots_perf(arch, dataset, exper_cntr, exper_no_cntr):
     print("plots saved in:", cntr_dir)
 
 
-def making_plots_eff(arch, dataset):
-    no_cntr_dir = f"../output/efficiency/{arch}/{dataset}/no_cntr/"
-    cntr_dir = f"../output/efficiency/{arch}/{dataset}/cntr/2/"
-    labels = ["w. $\phi$", "w.o. $\phi$"]
+def making_plots_eff(arch, dataset, exper_cntr, exper_no_cntr):
+    no_cntr_dir = f"../output/efficiency/{arch}/{dataset}/no_cntr/" + exper_no_cntr
+    cntr_dir = f"../output/efficiency/{arch}/{dataset}/cntr/" + exper_cntr
+    labels = [f"{arch} w. $\phi$", f"{arch} w.o. $\phi$"]
 
-    # "/home/soheil/Sync/umaine/bnn/nips/output/test/model/resnet/MNIST/cntr/"
+    if not os.path.exists(no_cntr_dir):
+        print(no_cntr_dir)
+        sys.exit(f"Directory for {arch} and {dataset} doesn't exist")
+
     args = json.load(open(cntr_dir + "exper.json"))
-    # print(args['train_epochs'])
 
     no_cntr_conn = pickle.load(open(no_cntr_dir + "conn.pkl", "rb"))
     no_cntr_all_acc = pickle.load(open(no_cntr_dir + "all_accuracies.pkl", "rb"))
@@ -92,7 +94,7 @@ def main():
     #         making_plots(arch, dataset)
 
 
-    # making_plots_eff(ARCHS[1], DATASETS[1])
+    making_plots_eff(ARCHS[1], DATASETS[1])
 
 if __name__ == '__main__':
     main()
