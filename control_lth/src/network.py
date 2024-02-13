f849316fd5b5568322a7ee1734134cd78b844141
Soheil Gharatappeh <soheilred@yahoo.com>
2024-02-13 13:57:03 -0500

    debug

--- control_lth/src/network.py
+++ control_lth/src/network.py
@@ -217,7 +217,6 @@ def train(model, dataloader, loss_fn, optimizer, mask, epochs, device):
                 # Set frozen param grads to 0.
                 # make_grads_zero(model, mask)
                 layer_id = 0
-                # assert self.current_masks
                 for name, param in model.named_parameters():
                     if 'weight' in name and param.dim() > 1:
                         param.grad *= mask[layer_id]
