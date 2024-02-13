d6211d32f5a1c9c6a50e7c57ef0401d408f45e2e
Soheil Gharatappeh <soheilred@yahoo.com>
2024-02-13 07:20:38 -0500

    debug

--- control_lth/src/prune.py
+++ control_lth/src/prune.py
@@ -493,9 +493,9 @@ def perf_lth(logger, device, args, controller):
         # except for the first iteration, we don't prune in the first iteration
         if imp_iter != 0:
             pruning.prune_once(init_state_dict)
-            non_frozen_parameters = [p for p in model.parameters() if p.requires_grad]
-            optimizer = torch.optim.Adam(non_frozen_parameters, lr=args.lr,
-                                         weight_decay=1e-4)
+            # non_frozen_parameters = [p for p in model.parameters() if p.requires_grad]
+            # optimizer = torch.optim.Adam(non_frozen_parameters, lr=args.lr,
+            #                              weight_decay=1e-4)
 
         logger.debug(f"[{imp_iter + 1}/{ITERATION}] " + "IMP loop")
 
