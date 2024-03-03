0aca7f22f5d88383adda34b091e2a84ba6dd022a
Soheil Gharatappeh <soheilred@yahoo.com>
2024-03-03 09:27:00 -0500

    no debug line

--- control_lth/src/prune.py
+++ control_lth/src/prune.py
@@ -526,10 +526,11 @@ def perf_lth(logger, device, args, controller):
             if (train_iter == controller.c_epoch) and \
                (imp_iter in controller.c_iter):
                 act = Activations(model, train_dl, device, args.batch_size)
-                corr_0, act_means_0 = act.get_corrs()
-                corr_1, act_means_1 = act.get_correlations()
-                import ipdb; ipdb.set_trace()
-                pruning.control(corr_1, act.layers_dim, imp_iter)
+                # corr_0, act_means_0 = act.get_corrs()
+                # corr_1, act_means_1 = act.get_correlations()
+                # import ipdb; ipdb.set_trace()
+                corr, act_means_1 = act.get_correlations()
+                pruning.control(corr, act.layers_dim, imp_iter)
                 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,
                                              weight_decay=1e-4)
 
