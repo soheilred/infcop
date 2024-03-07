73fb6ec3f2cee421eb2ea303cb60e13489b317ef
Soheil Gharatappeh <soheilred@yahoo.com>
2024-03-07 00:55:42 -0500

    change p and q

--- control_lth/src/prune.py
+++ control_lth/src/prune.py
@@ -257,7 +257,7 @@ class Pruner:
         pivot_param = torch.cat(pivot_param, dim=0).data.abs()
         pivot_mask = torch.cat(pivot_mask, dim=0)
         # p, q, eta_m, gamma = self.prune_mode[1:] # TODO
-        p, q, eta_m, gamma = float(1.0), float(2.0), float(0.), float(1.)
+        p, q, eta_m, gamma = float(0.5), float(1.0), float(0.), float(1.)
         beta = 0.9
         sparsity_index = {"p": torch.arange(0.1, 1.1, 0.1),
                           "q": torch.arange(1.0, 2.1, 0.1)}
