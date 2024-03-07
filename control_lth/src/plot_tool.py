030cad3c1d4afd3ca83dda666633ba3bb8dd78dd
Soheil Gharatappeh <soheilred@yahoo.com>
2024-03-07 16:57:31 -0500

    change color

--- control_lth/src/plot_tool.py
+++ control_lth/src/plot_tool.py
@@ -224,7 +224,7 @@ def plot_correlations(filename):
                  # marker=filled_markers[i],
                  # linestyle=linestyles[i % len(linestyles)],
                  # label=f"Iter {i}",
-                 c=f'#00{i*10:0>4}',
+                 c=f'#F0{i*10:0>4}',
                  lw=1,
                  alpha=.4)
     fig.tight_layout(pad=2.0)
