65c3b433f1a919f7782f5ec1a23b87aac7845d27
Soheil <soheilred@yahoo.com>
2024-08-05 13:25:23 -0400

    db

--- control_lth/src/plot_tool.py
+++ control_lth/src/plot_tool.py
@@ -552,9 +552,9 @@ def plot_similarity(exper_dir, vars=None):
         axs[i, 0].set(xlabel="Layer index", ylabel="Connectivity")
 
         axs[i, 0].grid()
-        fig.colorbar(colors, ax=conn_plt)
+        # fig.colorbar(colors, ax=conn_plt)
 
-    fig.colorbar(conn_plt, cax=cax, orientation='vertical')#.set_label("Epochs")
+    # fig.colorbar(conn_plt, cax=cax, orientation='vertical')#.set_label("Epochs")
     # fig.colorbar(conn_fig[3], colors).set_label("Epochs")
 
     # Gradient flow
