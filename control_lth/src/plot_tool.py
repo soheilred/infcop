2c7490a11b2f7c76faaad23c18abab1d8e605b1d
Soheil <soheilred@yahoo.com>
2024-08-05 13:51:27 -0400

    db

--- control_lth/src/plot_tool.py
+++ control_lth/src/plot_tool.py
@@ -592,7 +592,7 @@ def plot_similarity(exper_dir, vars=None):
         divider = make_axes_locatable(axs[i, 1])
         cax = divider.append_axes('right', size='5%', pad=0.05)
         cbar = fig.colorbar(cmap, cax=cax, ticks=[-1, 0, 1])
-        cbar.ax.setxticklabels(list(range(0, train_epochs, train_epochs // 5)))
+        cbar.ax.set_xticklabels(list(range(0, train_epochs, train_epochs // 5)))
     # y
     print("y: ", len(acc[0][0]))
     for i in range(imp_iter):
